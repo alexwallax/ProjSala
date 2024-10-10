b19b27470354011bb935cbca3c053e4a247538c4
Alexwallax <alexwallax@gmai.com>
2024-10-10 15:20:38 -0300

    Autenticacao

--- tarefa/views.py
+++ tarefa/views.py
@@ -2,32 +2,45 @@ from django.http import JsonResponse
 from rest_framework import viewsets, generics
 from tarefa.models import Tarefa, Comment, Tag, Notificacao, Anexo
 from tarefa.serializer import TarefaSerializers, ComentarioSerializers, TagSerializers, NotificacaoSerializers, AnexoSerializers, ListaComentarioSerializer, ListaTagSerializer
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class TarefaViewSet(viewsets.ModelViewSet):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializers
+    authentication_classes = [BasicAuthentication]
+    Permission_classes = [IsAuthenticated]
 
 class TarefaRetriveView(generics.RetrieveAPIView):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializers
+    authentication_classes = [BasicAuthentication]
+    Permission_classes = [IsAuthenticated]
 
 class ComentarioViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = ComentarioSerializers
+    authentication_classes = [BasicAuthentication]
+    Permission_classes = [IsAuthenticated]
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializers
+    authentication_classes = [BasicAuthentication]
+    Permission_classes = [IsAuthenticated]
 
 class NotificacaoViewSet(viewsets.ModelViewSet):
     queryset = Notificacao.objects.all()
     serializer_class = NotificacaoSerializers
+    authentication_classes = [BasicAuthentication]
+    Permission_classes = [IsAuthenticated]
 
 class AnexoViewSet(viewsets.ModelViewSet):
     queryset = Anexo.objects.all()
     serializer_class = AnexoSerializers
+    authentication_classes = [BasicAuthentication]
+    Permission_classes = [IsAuthenticated]
 
 class ListaComentarioTarefa(generics.ListAPIView):
     ''''Listando os comentários por taks'''
@@ -35,6 +48,8 @@ class ListaComentarioTarefa(generics.ListAPIView):
         queryset = Comment.objects.filter(fk_task=self.kwargs['pk'])
         return queryset 
     serializer_class = ListaComentarioSerializer
+    authentication_classes = [BasicAuthentication]
+    Permission_classes = [IsAuthenticated]
 
 
 class ListaTagTarefa(generics.ListAPIView):
@@ -43,3 +58,8 @@ class ListaTagTarefa(generics.ListAPIView):
         queryset = Tag.objects.filter(fk_task=self.kwargs['pk'])
         return queryset 
     serializer_class = ListaTagSerializer
+    authentication_classes = [BasicAuthentication]
+    Permission_classes = [IsAuthenticated]
+
+
+
